a5911ef65b6397ebbe19295fa1f9eaa00fbd5b4c
ivan-shrimp <70307174+ivan-shrimp@users.noreply.github.com>
2022-03-21 19:35:43 +0800

    improve `U32Reader` speed and API

--- read_u32/src/lib.rs
+++ read_u32/src/lib.rs
@@ -1,68 +1,137 @@
-use std::cell::Cell;
+use std::io;
 
-#[derive(Default)]
-pub struct U32Reader {
-    buf: Cell<Vec<u8>>,
+/// An adapter over a reader that reads `u32`s from it.
+#[derive(Clone, Debug)]
+pub struct U32Reader<R: io::BufRead> {
+    buf: Vec<u8>,
+    reader: R,
 }
 
-impl U32Reader {
-    /// Create a new reader.
+impl U32Reader<io::StdinLock<'static>> {
+    /// Creates a new adapter that reads `u32`s from `stdin`.
+    ///
+    /// This locks `stdin`, and the lock will only be released
+    /// when this `U32Reader` is dropped.
     #[must_use]
-    pub fn new() -> Self {
-        Self::default()
+    pub fn with_stdin() -> Self {
+        Self::new(io::stdin().lock())
+    }
+}
+
+impl<R: io::BufRead> U32Reader<R> {
+    /// Creates a new adapter that reads `u32`s from the given reader.
+    #[must_use]
+    pub const fn new(reader: R) -> Self {
+        Self {
+            buf: Vec::new(),
+            reader,
+        }
     }
 
-    /// Reads an unsigned integer from stdin that is known to end with a specific delimter.
-    /// For example, use `b' '` for space-separated integers and `b'\n'` for integers separated by newlines.
+    /// Returns a mutable reference to the underlying reader.
+    pub fn get_mut(&mut self) -> &mut R {
+        &mut self.reader
+    }
+
+    /// Unwraps the adapter, returning the underlying reader.
+    // This clippy lint is a false positive; compiler complains
+    // "destructors cannot be evaluated at compile-time" if `const` is added.
+    // This is because we have to drop the buffer.
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn into_inner(self) -> R {
+        self.reader
+    }
+
+    /// Reads a `u32` from the underlying reader in a zero-copy manner.
     ///
-    /// # Panics
+    /// Use only when the delimiter is guaranteed to be a single byte,
+    /// meaning that this is unsuitable for reading integers terminated by a newline,
+    /// because that can end with either `\n` or `\r\n`.
     ///
-    /// Panics when there are no more integers in stdin,
-    /// when any I/O error occurs,
-    /// and when the integer cannot be represented by a u32.
+    /// May return None if the underlying buffer of the reader does not contain any delimiter.
+    /// One may use `read_until_fallback` in that case.
+    ///
+    /// Panics if no valid digits can be obtained from the underlying reader,
+    /// if the integer cannot fit into a `u32`,
+    /// or if any I/O error occured.
     #[must_use]
-    pub fn read_until(&self, delim: u8) -> u32 {
-        use std::io::{self, BufRead};
+    fn read_until_fast(&mut self) -> Option<u32> {
+        loop {
+            // Get the internal buffer of the underying reader.
+            let available = match self.reader.fill_buf() {
+                Ok(n) => n,
+                Err(ref e) if e.kind() == io::ErrorKind::Interrupted => continue,
+                Err(e) => {
+                    panic!("An I/O error occured when attempting to read from stdin: {e:?}")
+                }
+            };
 
-        // If any panic occurs below, the internal buffer becomes Vec::new().
-        let mut buf = self.buf.take();
+            // Parse the first bytes of the buffer into an integer.
+            // `parse_partial` returns both the parsed integer and, implicitly, the position of the delimiter.
+            let (int, read) = lexical::parse_partial::<u32, _>(available)
+                .expect("An error occured when parsing an input integer");
 
-        // Clear the buffer for writing new data into it.
-        buf.clear();
+            // If any delimiter was found during parsing, return the parsed integer.
+            // Otherwise, the integer might be split on the boundary between two buffers, so we should return None.
+            return (read < available.len()).then(|| {
+                // Consume the integer and the delimiter byte.
+                self.reader.consume(read + 1);
+                int
+            });
+        }
+    }
 
-        let written = io::stdin()
-            // Acquire a lock to stdin.
-            .lock()
+    /// Reads a `u32` from the underlying reader
+    /// by copying data from the reader into the internal buffer of this `U32Reader`.
+    ///
+    /// Can be used even if there are any other bytes between the digits and the delimiter,
+    /// or if the digits are directly followed by EOF.
+    /// For example, if the reader contains `123\r\n` and `delim` is set to `\n`, `123` will be correctly returned.
+    ///
+    /// Panics if no valid digits can be obtained from the underlying reader,
+    /// if the integer cannot fit into a `u32`,
+    /// or if any I/O error occured.
+    #[must_use]
+    fn read_until_fallback(&mut self, delim: u8) -> u32 {
+        // Clear the internal buffer for writing new data into it.
+        self.buf.clear();
+
+        let used = self
+            .reader
             // Read the decimal-encoded number into the buffer.
-            .read_until(delim, &mut buf)
+            .read_until(delim, &mut self.buf)
             // Panic if any I/O error occured.
             .expect("An I/O error occured when attempting to read from stdin");
 
-        assert_ne!(written, 0, "No more integers can be read from stdin");
+        assert_ne!(used, 0, "No more integers can be read from stdin");
 
-        // Convert the input into an integer.
-        // `parse_partial`, unlike the standard library `parse`,
-        // can ignore the delimiter(s) left by `read_until` at the end of the string.
-        let result = lexical::parse_partial(&buf)
+        // Parse the input into an integer.
+        // `parse_partial` ignores the delimiter(s) left by `read_until` at the end of the string.
+        lexical::parse_partial(&self.buf)
             .expect("Input cannot be represented by an unsigned 32-bit integer")
-            .0;
-
-        // Reuse the internal buffer for next read.
-        self.buf.set(buf);
-
-        result
+            .0
     }
 
-    /// Read one line of space-separated integers from stdin.
+    /// Reads a `u32`, that is known to terminate with a space character (`' '`),
+    /// from the underlying reader.
     ///
-    /// # Panics
+    /// Panics if no valid digits can be obtained from the underlying reader,
+    /// if the integer cannot fit into a `u32`,
+    /// or if any I/O error occured.
+    #[must_use]
+    pub fn read_until_space(&mut self) -> u32 {
+        self.read_until_fast()
+            .unwrap_or_else(|| self.read_until_fallback(b' '))
+    }
+
+    /// Reads a `u32`, that is known to terminate with a newline (`\n` or `\r\n`),
+    /// from the underlying reader.
     ///
-    /// See `read_until` for detailed semantics.
-    pub fn read_line(&self, n: usize) -> impl Iterator<Item = u32> + '_ {
-        (0..n - 1)
-            // The first `n - 1` integers are terminated by a space character.
-            .map(|_| self.read_until(b' '))
-            // The last integer is terminated by a newline character (or EOF).
-            .chain(std::iter::once_with(|| self.read_until(b'\n')))
+    /// Panics if no valid digits can be obtained from the underlying reader,
+    /// if the integer cannot fit into a `u32`,
+    /// or if any I/O error occured.
+    #[must_use]
+    pub fn read_until_newline(&mut self) -> u32 {
+        self.read_until_fallback(b'\n')
     }
 }
